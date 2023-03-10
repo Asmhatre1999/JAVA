3c1f989512a06065d22c34957ec56f84d36512ec
Archita mhatre <31178159+Asmhatre1999@users.noreply.github.com>
2023-03-10 15:00:29 +0530

    Add files via upload

---
+++ Practicals/TestCGPAException.java
@@ -0,0 +1,19 @@
+
+public class TestCGPAException {
+	public static void main(String[] args) {
+		try {
+			checkCGPA(6);
+		} catch (LowCGPAException e) {
+			System.out.println(e);
+		}
+	}
+
+	public static void checkCGPA(int i) throws LowCGPAException {
+		if (i < 7) {
+			throw new LowCGPAException("You are not eligible for placements.");
+		}else {
+			System.out.println("You are eligible for placements.");
+		}
+	}
+
+}
